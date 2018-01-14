f9f602b6a21ecf9fade633aef2d51e5e39b84703
bealk <bea.lk@note.pl>
2018-01-14 19:06:05 +0100

    Initial commit

---
+++ server.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const hbs = require('hbs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+
+app.use((req, res, next) => {
+	var now = new Date().toString();
+	console.log(`${now}: ${req.method} ${req.url}`)
+	next();
+});
+
+hbs.registerHelper('getCurrentYear', () => {
+	return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+	return text.toUpperCase();
+});
+
+app.get('/', (req, res) => {
+	res.render('home.hbs',{
+		pageTitle: 'Home Page',		
+		welcomeMessage: 'Welcome to my site'
+	});
+});
+
+// app.get('/', (req, res) => {
+// 	res.send({
+// 		name: 'Andrew',
+// 		likes: [
+// 			'Biking',
+// 			'Cities'
+// 		]
+// 	});
+// });
+
+app.get('/about', (req, res) => {
+	res.render('about.hbs', {
+		pageTitle: 'About Page',
+	});
+});
+
+app.get('/bad', (req, res) => {
+	res.send({
+		errorMessage: 'Unable to handle request'
+	});
+});
+
+app.listen(3000, () => console.log('server has started'));
